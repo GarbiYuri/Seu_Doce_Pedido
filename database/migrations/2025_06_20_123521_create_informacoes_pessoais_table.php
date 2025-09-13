6c2641ef89dc9bad2b1bf5cab952bf00e8aba3f8
GArbiYuri <franciscoyuri145@gmail.com>
2025-09-13 12:19:29 -0300

    logica 1+ endereco

--- database/migrations/2025_06_20_123521_create_informacoes_pessoais_table.php
+++ database/migrations/2025_06_20_123521_create_informacoes_pessoais_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('informacoes_pessoais', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->string('nome')->nullable();
             $table->string('rua')->nullable();
             $table->string('numero')->nullable();
             $table->string('bairro')->nullable();
