5e8142406de027f5635fdd84c2d089d658111f67
GArbiYuri <franciscoyuri145@gmail.com>
2025-10-29 01:01:04 -0300

    linkando venda com o sistema

--- database/migrations/2025_10_28_210959_create_cupom_user_table.php
+++ database/migrations/2025_10_28_210959_create_cupom_user_table.php
@@ -15,13 +15,11 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('cupom_id');
-            $table->unsignedBigInteger('venda_id');
             $table->integer('usos')->default(0);
             $table->timestamps();
             
             
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('venda_id')->references('id')->on('vendas')->onDelete('cascade');
             $table->foreign('cupom_id')->references('id')->on('cupons')->onDelete('cascade');
             
             $table->unique(['user_id', 'cupom_id']);
